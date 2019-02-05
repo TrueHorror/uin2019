5a2ed7834e8691693771203e2acf65f0a76a0542
Unknown <fredrik.olsvold@gmail.com>
2019-02-05 02:41:09 +0100

    update

--- obliger/Oblig1/forestgump.php
+++ obliger/Oblig1/forestgump.php
@@ -1,3 +1,7 @@
 <?php
-
+$bilde = "img/interstellar.jpg";
+$titel = "interstellar";
+$info = "Filmen utspiller seg i SPACE!";
+$skuespiller = "Matthew McConaughey";
+$imdb = "https://www.imdb.com/title/tt0816692/?ref_=nv_sr_1";
 ?>
--- obliger/Oblig1/index.php
+++ obliger/Oblig1/index.php
@@ -3,9 +3,25 @@
   <head>
     <title>Favoritt filmer</title>
   </head>
-  <body> 
+  <body>
+  	
+  	<?php
+      require("interstellar.php");
+      require("thedarkknight.php");
+      require("forestgump.php");
+     ?> 
   	 <nav class="meny">
   	 	<?php
+  	 	switch (variable) {
+  	 		case 'value':
+  	 			# code...
+  	 			break;
+  	 		
+  	 		default:
+  	 			# code...
+  	 			break;
+  	 	}
+  	 		echo "<a href='#'>" . $titel .   
   	 		//fylle menyen med antall filmer/filer
   	 	  ?>
   	 </nav>
