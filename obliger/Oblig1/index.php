39780237f9c81b4b7c02964f3ee407a2323db05e
Unknown <fredrik.olsvold@gmail.com>
2019-02-05 02:32:42 +0100

    update

---
+++ obliger/Oblig1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Favoritt filmer</title>
+  </head>
+  <body> 
+  	 <nav class="meny">
+  	 	<?php
+  	 		//fylle menyen med antall filmer/filer
+  	 	  ?>
+  	 </nav>
+  </body>
+</html>
